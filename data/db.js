f4f8c7ef49dd56a9715002feec1ab26018e66bb8
shaniaesguerra <shaniae47@gmail.com>
2026-07-03 13:49:41 -0600

    Connecting node project to MongoDB

---
+++ data/db.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+
+let database;
+
+const initDb = (callback) => {
+    //if database is set-up:
+    if (database) {
+        console.log('Database is already initialized!');
+        return callback(null, database);
+    }
+    //otherwise connect to the db:
+    MongoClient.connect(process.env.MONGO_URI)
+        .then((client) => {
+            database = client.db();
+            callback(null, database);
+        })
+        .catch((err) => {
+            console.error(err);
+            callback(err, null);
+        });
+};
+
+const getDatabase = () => {
+    //if database is not initialized:
+    if (!database) {
+        //throw an error:
+        throw Error('Database not initialized');
+    }
+    //otherwise, return the database
+    return database;
+};
+
+module.exports = { initDb, getDatabase };
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const app = express();
+const mongodb = require('./data/db');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 //Middlewares
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node is running on port ${port}`);
+    });
+  }
 });
