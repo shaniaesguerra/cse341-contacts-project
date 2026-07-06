05072b67a7396f4d180ed516f4ce08043e6ed52f
shaniaesguerra <shaniae47@gmail.com>
2026-07-06 12:23:21 -0600

    changed message on get '/'

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
-    res.send('For all users, go to /users and for a single user, go to /users/:id');
+    res.send('Hello World!<br>For <strong>all users</strong>, go to <strong>/users</strong><br>or a <strong>single user</strong>, go to <strong>/users/:id</strong>');
 });
 
 router.use('/users', require('./users'));
