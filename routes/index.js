8f6e44fd3abfd63befc1c47a9b308a119eca1f4f
shaniaesguerra <shaniae47@gmail.com>
2026-07-06 12:18:40 -0600

    added message on get '/'

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
+    res.send('For all users, go to /users and for a single user, go to /users/:id');
 });
 
 router.use('/users', require('./users'));
