d10948a0161bc2006dfc3e3c8f0aad72ded2f04d
shaniaesguerra <shaniae47@gmail.com>
2026-07-06 12:48:01 -0600

    changed file names

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!<br>For <strong>all users</strong>, go to <strong>/users</strong><br>or a <strong>single user</strong>, go to <strong>/users/:id</strong>');
+    res.send('Hello World!<br>For <strong>all contacts</strong>, go to <strong>/contacts</strong><br>or a <strong>single contact</strong>, go to <strong>/contacts/:id</strong>');
 });
 
-router.use('/users', require('./users'));
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
